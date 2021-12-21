394034d7a30e274cdef9216fd931b8c2b9ecb7d8
diganthsanghvi17 <diganthsanghvi@outlook.com>
2021-12-22 01:57:27 +1100

    first question solved

--- numbers.py
+++ numbers.py
@@ -1,2 +1,12 @@
 # this is a python project to learn python from scratch with version control
 # this exercise is checking version control and coding in python
+
+#You have a football field that is 92 meter long and 48.8 meter wide. Find out total area using python and print it.
+
+#create the variables
+length = 92
+breath = 48.8
+# write the formula for area
+area = length * breath
+# print the output
+print(area)
